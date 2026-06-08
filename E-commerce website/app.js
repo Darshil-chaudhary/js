de04f21652663eae1ea22701253a49bb748d7179
Darshil chaudhry <darshilvinodbhai@gmail.com>
2026-06-09 00:38:15 +0530

    shopcart web

---
+++ E-commerce website/app.js
@@ -0,0 +1,88 @@
+let products = [
+    {
+        name: "Wireless Earbuds IPX8",
+        price: 89,
+        description: "Organic Cotton, Airtight certified",
+        rating: 4.8,
+        image: "https://cpimg.tistatic.com/12118915/b/5/FirePods-4-True-Wireless-Earbuds-with-HD-Sound-Deep-Bass-ENC-Low-Latency-Long-Battery-Life.jpeg"
+    },
+    {
+        name: "AirPods Max",
+        price: 559,
+        description: "A perfect balance of high-fidelity audio",
+        rating: 4.9,
+        image: "https://i5.walmartimages.com/asr/bb909144-c02c-49ec-9a37-e50fd494c8fb.00b36dc40544611dfe881a831e1c90db.jpeg"
+    },
+    {
+        name: "Bose BT Earphones",
+        price: 289,
+        description: "Table with air purifier, stained veneer",
+        rating: 4.7,
+        image: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=1080/da/cms-assets/cms/product/b31feb49-d0cd-4526-9684-6110bfeb5072.png"
+    },
+    {
+        name: "VIVEFOX Headphones",
+        price: 39,
+        description: "Wired Stereo Headsets With Mic",
+        rating: 4.6,
+        image: "https://5.imimg.com/data5/SELLER/Default/2021/5/ZE/FV/KC/68213536/thump-fwh-225-black-red.jpg"
+    },
+    {
+        name: "JBL TUNE 600BTNC",
+        price: 59,
+        description: "Premium Bone Conduction Open Ear Bluetooth",
+        rating: 4.8,
+        image: "https://i5.walmartimages.com/asr/bb909144-c02c-49ec-9a37-e50fd494c8fb.00b36dc40544611dfe881a831e1c90db.jpeg"
+    },
+    {
+        name: "TAGRY Bluetooth",
+        price: 109,
+        description: "256 GB, 8 Core GPU, 8 GB RAM",
+        rating: 4.7,
+        image: "https://images.price.tools/images/tagry-bluetooth-earbuds-5-4-40hrs-l-jVonmb1i2.jpg"
+    },
+    {
+        name: "Monster MNFLEX",
+        price: 89,
+        description: "Flex Active Noise Cancelling Bluetooth",
+        rating: 4.5,
+        image: "https://m.media-amazon.com/images/I/71oS0bdWgqL.jpg_BO30,255,255,255_UF750,750_SR1910,1000,0,C_QL100_.jpg"
+    },
+    {
+        name: "Mpow CH6",
+        price: 569,
+        description: "Kids Headphones",
+        rating: 4.8,
+        image: "https://images-na.ssl-images-amazon.com/images/I/710ALPe%2BiPL._SS400_.jpg"
+    }
+];
+let product1 = [
+    {
+        name: "Gaming Headphone",
+        price: 239,
+        description: "Table with air purifier, stained venner/black",
+        rating: 4.8,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj0RNyiug4qIFThtk4y3Nag8QidQQaUp6qmA&s"
+    },
+    {
+        name: "MacBook Pro 13",
+        price: 1099,
+        description: "256 GB SSD, 8 GB RAM, Retina Display",
+        rating: 4.9,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMyyiOZTVIaTMHqXC7oEA6i3dcLP43kvaAhw&s"
+    },
+    {
+        name: "HomePod Mini",
+        price: 59,
+        description: "Smart speaker with immersive 360° sound",
+        rating: 4.8,
+        image: "https://cdn.jiostore.online/v2/jmd-asp/jdprod/wrkr/products/pictures/item/free/resize-w:450/apple/494443243/0/hCK3rsr9RJ-pwKQkcf-5v-Apple-HomePod-Mini-Speaker-Midnight-494443243-i-1-1200Wx1200H.jpeg"
+    },
+    {
+        name: "Laptop Sleeve MacBook",
+        price: 59,
+        description: "Organic cotton, fairtrade certified",
+        rating: 4.8,
+        image: "https://vsprod.vijaysales.com/media/catalog/product/2/4/245659_7_.jpg?optimize=medium&fit=bounds&height=500&width=500"
+    }
+];
